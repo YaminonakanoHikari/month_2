6f625f9849a5b8ffdba3615770306192c998987d
Ariana <mdmkillermdm@gmail.com>
2025-08-09 18:43:55 +0600

    finaly hw3

--- homework_3.py
+++ homework_3.py
@@ -1,12 +1,12 @@
 class Person:
     # Инициализация атрибутов экземпляра
-    def __init__(self, name, birth_date, occupation, higher_education):
+    def __init__(self, name, birth_date, age, occupation, higher_education):
         self.name = name                      # Имя
         self.birth_date = birth_date          # Дата рождения
-        self.__occupation = occupation        # Профессия (приватный атрибут)
-        self.__higher_education = higher_education  # Высшее образование (приватный атрибут True/False)
+        self.__age = age                        #Возраст
+        self.__occupation = occupation          # Профессия
+        self.__higher_education = higher_education  # Высшее образование (True/False)
 
-    # Геттер и сеттер для occupation
     @property
     def occupation(self):
         return self.__occupation
@@ -15,7 +15,6 @@ class Person:
     def occupation(self, value):
         self.__occupation = value
 
-    # Геттер и сеттер для higher_education
     @property
     def higher_education(self):
         return self.__higher_education
@@ -24,40 +23,46 @@ class Person:
     def higher_education(self, value):
         self.__higher_education = value
 
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, value):
+        self.__age = value
+
+
+
     # Базовый метод introduce
     def introduce(self):
-        edu_status = "У меня есть высшее образование." if self.higher_education else "У меня нет высшего образования."
-        print(f"Привет! Меня зовут {self.name}. Моя профессия {self.occupation}. {edu_status}")
-
+        edu_status = "с высшим образованием" if self.__higher_education else "без высшего образования"
+        print(f"Привет! Меня зовут {self.name}. Я родился {self.birth_date}, мне {self.__age} "
+              f"работаю {self.__occupation}, {edu_status}.")
 
 # Класс-наследник "Одноклассник"
 class Classmate(Person):
-    def __init__(self, name, birth_date, occupation, higher_education, group):
-        super().__init__(name, birth_date, occupation, higher_education)
-        self.group = group
-
     def introduce(self):
-        edu_status = "У меня есть высшее образование." if self.higher_education else "У меня нет высшего образования."
-        print(f"Привет! Меня зовут {self.name}. Моя профессия {self.occupation}. "
-              f"Я учился с Арианой в группе {self.group}. {edu_status}")
-
+        print(f"Привет! Меня зовут {self.name}, я одноклассник Игоря. "
+              f"Я родился {self.birth_date}, работаю {self.occupation}.")
 
 # Класс-наследник "Друг"
 class Friend(Person):
-    def __init__(self, name, birth_date, occupation, higher_education, hobby):
-        super().__init__(name, birth_date, occupation, higher_education)
-        self.hobby = hobby
-
     def introduce(self):
-        edu_status = "У меня есть высшее образование." if self.higher_education else "У меня нет высшего образования."
-        print(f"Привет! Меня зовут {self.name}. Моя профессия {self.occupation}. "
-              f"Мое хобби {self.hobby}. {edu_status}")
-
+        print(f"Привет! Меня зовут {self.name}, я друг Игоря. "
+              f"Я родился {self.birth_date}, работаю {self.occupation}.")
 
 # Создание объектов
-cl1 = Classmate("Иван", "20.02.2000", "студент", True, "11D")
-fr1 = Friend("Айбек", "20.02.2000", "студент", True, "футбол")
+classmate1 = Classmate("Бектур", "05.12.2000",25,"Программист",True)
+classmate1.introduce()
+print(classmate1.age)
+classmate2 = Classmate("Данияр", "14.08.2001",24, "Дизайнер", False)
+classmate2.introduce()
+print(classmate2.age)
 
-# Вызов метода introduce() для каждого объекта
-cl1.introduce()
-fr1.introduce()
+friend1 = Friend("Алмаз", "11.03.1999", 26,"Инженер", True)
+friend1.introduce()
+print(friend1.age)
+friend2 = Friend("Канат", "22.07.2000", 25,"Учитель", True)
+friend2.introduce()
+print(friend2.age)
+
