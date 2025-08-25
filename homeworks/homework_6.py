3c0f2350ed92393f9914f8813059907974f683da
Ariana <mdmkillermdm@gmail.com>
2025-08-25 16:02:32 +0600

    hw6

---
+++ homeworks/homework_6.py
@@ -0,0 +1,11 @@
+from blessed import Terminal
+
+term = Terminal()
+
+print(term.red + "Apple" + term.normal)
+print(term.darkolivegreen1 + "Banana" + term.normal)
+print(term.lightgoldenrod + "Cherry" + term.normal)
+print(term.paleturquoise + "Grape" + term.normal)
+print(term.slateblue2 + "Mango" + term.normal)
+print(term.aquamarine3 + "Orange" + term.normal)
+print(term.snow4 + "Peach" + term.normal)
--- homeworks/homework_7.py
+++ homeworks/homework_7.py
@@ -1,63 +1,36 @@
-class Person:
-    # Инициализация атрибутов экземпляра
-    def __init__(self, name, birth_date, occupation, higher_education):
-        self.name = name                      # Имя
-        self.birth_date = birth_date          # Дата рождения
-        self.__occupation = occupation        # Профессия (приватный атрибут)
-        self.__higher_education = higher_education  # Высшее образование (приватный атрибут True/False)
+import sqlite3
 
-    # Геттер и сеттер для occupation
-    @property
-    def occupation(self):
-        return self.__occupation
+def create_table():
+    conn.execute('''CREATE TABLE IF NOT EXISTS books(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    author TEXT,
+    publication_year INTEGER,
+    genre TEXT,
+    number_of_pages INTEGER,
+    number_of_copies INTEGER
+    )
+    ''')
 
-    @occupation.setter
-    def occupation(self, value):
-        self.__occupation = value
-
-    # Геттер и сеттер для higher_education
-    @property
-    def higher_education(self):
-        return self.__higher_education
-
-    @higher_education.setter
-    def higher_education(self, value):
-        self.__higher_education = value
-
-    # Базовый метод introduce
-    def introduce(self):
-        edu_status = "У меня есть высшее образование." if self.higher_education else "У меня нет высшего образования."
-        print(f"Привет! Меня зовут {self.name}. Моя профессия {self.occupation}. {edu_status}")
+def insert_books(name, author, publication_year, genre, number_of_pages, number_of_copies):
+    conn.execute('''INSERT INTO books(name, author, publication_year, 
+                                      genre, number_of_pages, number_of_copies) VALUES(?,?,?,?,?,?) 
+                 ''', (name, author, publication_year, genre, number_of_pages, number_of_copies))
+    conn.commit()
 
 
-# Класс-наследник "Одноклассник"
-class Classmate(Person):
-    def __init__(self, name, birth_date, occupation, higher_education, group):
-        super().__init__(name, birth_date, occupation, higher_education)
-        self.group = group
-
-    def introduce(self):
-        edu_status = "У меня есть высшее образование." if self.higher_education else "У меня нет высшего образования."
-        print(f"Привет! Меня зовут {self.name}. Моя профессия {self.occupation}. "
-              f"Я учился с Арианой в группе {self.group}. {edu_status}")
 
 
-# Класс-наследник "Друг"
-class Friend(Person):
-    def __init__(self, name, birth_date, occupation, higher_education, hobby):
-        super().__init__(name, birth_date, occupation, higher_education)
-        self.hobby = hobby
 
-    def introduce(self):
-        edu_status = "У меня есть высшее образование." if self.higher_education else "У меня нет высшего образования."
-        print(f"Привет! Меня зовут {self.name}. Моя профессия {self.occupation}. "
-              f"Мое хобби {self.hobby}. {edu_status}")
+if  __name__ == '__main__':
+    conn = sqlite3.connect('data.db')
+    insert_books('name', 'author', 56, 'genre', 56, 56)
 
 
-# Создание объектов
-cl1 = Classmate("Иван", "20.02.2000", "студент", True, "11D")
-fr1 = Friend("Айбек", "20.02.2000", "студент", True, "футбол")
 
-# Вызов метода introduce() для каждого объекта
-cl1.introduce()
-fr1.introduce()
+
+
+
+
+
+    create_table()
